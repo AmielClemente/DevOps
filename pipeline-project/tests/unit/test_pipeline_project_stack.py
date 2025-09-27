e19dd3680931446033bcb381e0d524e8417ac135
AmielClemente <22070210@student.westernsydney.edu.au>
2025-09-27 17:26:47 +1000

    Fix import error in test file - update to PipelineProjectStackV2

--- pipeline-project/tests/unit/test_pipeline_project_stack.py
+++ pipeline-project/tests/unit/test_pipeline_project_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from pipeline_project.pipeline_project_stack import PipelineProjectStack
+from pipeline_project.pipeline_project_stack import PipelineProjectStackV2
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in pipeline_project/pipeline_project_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = PipelineProjectStack(app, "pipeline-project")
+    stack = PipelineProjectStackV2(app, "pipeline-project")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
